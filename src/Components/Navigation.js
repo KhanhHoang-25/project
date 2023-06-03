f123814ff7340a0de6f2f426a84b8e7a6ac431a4
Uyen Nguyen <uyennhkse160055@fpt.edu.vn>
2023-06-03 16:32:55 +0700

    Ex10 done

---
+++ src/Components/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+export default function Navigation() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home" className="active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
